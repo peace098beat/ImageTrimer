961d68b5edfb852a38660b4c135003a0c804668c
peace098beat <peace098beat.t@gmail.com>
2017-12-11 22:55:22 +0900

    Implimented Triming

--- CSharp-Trimer/Form1.Designer.cs
+++ CSharp-Trimer/Form1.Designer.cs
@@ -28,20 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.button_LoadImage = new System.Windows.Forms.Button();
+            this.label_LoadImage = new System.Windows.Forms.Label();
+            this.label_Savedir = new System.Windows.Forms.Label();
+            this.button_SaveDir = new System.Windows.Forms.Button();
+            this.pictureBox_OriginImage = new System.Windows.Forms.PictureBox();
+            this.listView_TrimedImages = new System.Windows.Forms.ListView();
+            this.button_Triming = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button_ListUpdate = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Debug = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_OriginImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button_LoadImage
+            // 
+            this.button_LoadImage.Location = new System.Drawing.Point(12, 12);
+            this.button_LoadImage.Name = "button_LoadImage";
+            this.button_LoadImage.Size = new System.Drawing.Size(75, 23);
+            this.button_LoadImage.TabIndex = 0;
+            this.button_LoadImage.Text = "button1";
+            this.button_LoadImage.UseVisualStyleBackColor = true;
+            this.button_LoadImage.Click += new System.EventHandler(this.button_LoadImage_Click);
+            // 
+            // label_LoadImage
+            // 
+            this.label_LoadImage.AutoSize = true;
+            this.label_LoadImage.Location = new System.Drawing.Point(93, 17);
+            this.label_LoadImage.Name = "label_LoadImage";
+            this.label_LoadImage.Size = new System.Drawing.Size(35, 12);
+            this.label_LoadImage.TabIndex = 1;
+            this.label_LoadImage.Text = "label1";
+            // 
+            // label_Savedir
+            // 
+            this.label_Savedir.AutoSize = true;
+            this.label_Savedir.Location = new System.Drawing.Point(409, 22);
+            this.label_Savedir.Name = "label_Savedir";
+            this.label_Savedir.Size = new System.Drawing.Size(35, 12);
+            this.label_Savedir.TabIndex = 3;
+            this.label_Savedir.Text = "label1";
+            // 
+            // button_SaveDir
+            // 
+            this.button_SaveDir.Location = new System.Drawing.Point(328, 17);
+            this.button_SaveDir.Name = "button_SaveDir";
+            this.button_SaveDir.Size = new System.Drawing.Size(75, 23);
+            this.button_SaveDir.TabIndex = 2;
+            this.button_SaveDir.Text = "button1";
+            this.button_SaveDir.UseVisualStyleBackColor = true;
+            this.button_SaveDir.Click += new System.EventHandler(this.button_SaveDir_Click);
+            // 
+            // pictureBox_OriginImage
+            // 
+            this.pictureBox_OriginImage.Location = new System.Drawing.Point(12, 115);
+            this.pictureBox_OriginImage.Name = "pictureBox_OriginImage";
+            this.pictureBox_OriginImage.Size = new System.Drawing.Size(291, 267);
+            this.pictureBox_OriginImage.TabIndex = 4;
+            this.pictureBox_OriginImage.TabStop = false;
+            // 
+            // listView_TrimedImages
+            // 
+            this.listView_TrimedImages.Location = new System.Drawing.Point(494, 101);
+            this.listView_TrimedImages.Name = "listView_TrimedImages";
+            this.listView_TrimedImages.Size = new System.Drawing.Size(294, 328);
+            this.listView_TrimedImages.TabIndex = 5;
+            this.listView_TrimedImages.UseCompatibleStateImageBehavior = false;
+            // 
+            // button_Triming
+            // 
+            this.button_Triming.Location = new System.Drawing.Point(13, 388);
+            this.button_Triming.Name = "button_Triming";
+            this.button_Triming.Size = new System.Drawing.Size(290, 26);
+            this.button_Triming.TabIndex = 6;
+            this.button_Triming.Text = "Triming";
+            this.button_Triming.UseVisualStyleBackColor = true;
+            this.button_Triming.Click += new System.EventHandler(this.button_Triming_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button_ListUpdate
+            // 
+            this.button_ListUpdate.Location = new System.Drawing.Point(328, 56);
+            this.button_ListUpdate.Name = "button_ListUpdate";
+            this.button_ListUpdate.Size = new System.Drawing.Size(75, 23);
+            this.button_ListUpdate.TabIndex = 7;
+            this.button_ListUpdate.Text = "UPDATE";
+            this.button_ListUpdate.UseVisualStyleBackColor = true;
+            this.button_ListUpdate.Click += new System.EventHandler(this.button_ListUpdate_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(69, 41);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            224,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            14,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 19);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            112,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "stride";
+            // 
+            // textBox_Debug
+            // 
+            this.textBox_Debug.Location = new System.Drawing.Point(13, 432);
+            this.textBox_Debug.Name = "textBox_Debug";
+            this.textBox_Debug.Size = new System.Drawing.Size(290, 19);
+            this.textBox_Debug.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(288, 263);
+            this.ClientSize = new System.Drawing.Size(834, 513);
+            this.Controls.Add(this.textBox_Debug);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button_ListUpdate);
+            this.Controls.Add(this.button_Triming);
+            this.Controls.Add(this.listView_TrimedImages);
+            this.Controls.Add(this.pictureBox_OriginImage);
+            this.Controls.Add(this.label_Savedir);
+            this.Controls.Add(this.button_SaveDir);
+            this.Controls.Add(this.label_LoadImage);
+            this.Controls.Add(this.button_LoadImage);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_OriginImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_LoadImage;
+        private System.Windows.Forms.Label label_LoadImage;
+        private System.Windows.Forms.Label label_Savedir;
+        private System.Windows.Forms.Button button_SaveDir;
+        private System.Windows.Forms.PictureBox pictureBox_OriginImage;
+        private System.Windows.Forms.ListView listView_TrimedImages;
+        private System.Windows.Forms.Button button_Triming;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button_ListUpdate;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Debug;
     }
 }
 
--- CSharp-Trimer/Form1.cs
+++ CSharp-Trimer/Form1.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,260 @@ namespace CSharp_Trimer
 {
     public partial class Form1 : Form
     {
+
+        string OriginFilePath;
+        string SaveDirPath;
+
+
         public Form1()
         {
             InitializeComponent();
+
+            this.OriginFilePath = @"C:\Users\USER\Downloads\Hydrangeas.jpg";
+            SetOriginImage(OriginFilePath);
+
+            this.SaveDirPath = @"C:\Users\USER\Desktop\trimed";
+            //UpdateSaveDir();
+
+        }
+
+        void SetOriginImage(string origin_file_path)
+        {
+            this.OriginFilePath = origin_file_path;
+            this.label_LoadImage.Text = this.OriginFilePath;
+            this.pictureBox_OriginImage.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBox_OriginImage.Image = Image.FromFile(this.OriginFilePath);
+        }
+
+        void UpdateSaveDir()
+        {
+
+
+        }
+
+
+        ////////////////////////////////////////////////////
+        // サブルーチン
+
+        // 幅w、高さhのImageオブジェクトを作成
+        Image createThumbnail(Image image, int w, int h)
+        {
+            Bitmap canvas = new Bitmap(w, h);
+
+            Graphics g = Graphics.FromImage(canvas);
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, w, h);
+
+            float fw = (float)w / (float)image.Width;
+            float fh = (float)h / (float)image.Height;
+
+            float scale = Math.Min(fw, fh);
+            fw = image.Width * scale;
+            fh = image.Height * scale;
+
+            g.DrawImage(image, (w - fw) / 2, (h - fh) / 2, fw, fh);
+            g.Dispose();
+
+            return canvas;
+        }
+
+
+        private void button_LoadImage_Click(object sender, EventArgs e)
+        {
+
+            // 1. Load Image
+            // 2. Show Image Info
+
+
+
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            ofd.FileName = "default.html";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            //ofd.Filter = "HTMLファイル(*.html;*.htm)|*.html;*.htm|すべてのファイル(*.*)|*.*";
+            //ofd.Filter = "Imageファイル(*.html;*.htm)|*.jpg;*.jpeg|すべてのファイル(*.*)|*.*";
+            ofd.Filter = "画像ファイル(*.png,*.jpg,*.bmp,*.gif)|*.png;*.jpg;*.bmp;*.gif|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに選択されるものを指定する
+            //2番目の「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 1;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき、選択されたファイル名を表示する
+                Console.WriteLine(ofd.FileName);
+                // Origin画像をセット
+                this.SetOriginImage(ofd.FileName);
+            }
+
+
+        }
+
+        // crop using Graphics object
+        Bitmap Crop1(Bitmap image)
+        {
+            Bitmap dest = new Bitmap(200, 150, image.PixelFormat);
+            Graphics g = Graphics.FromImage(dest);
+            Rectangle srcRect = new Rectangle(50, 80, 200, 150);
+            Rectangle desRect = new Rectangle(0, 0, 200, 150);
+            g.DrawImage(image, desRect, srcRect, GraphicsUnit.Pixel);
+            g.Dispose();
+            return dest;
+        }
+
+        // crop using Bitmap.Clone method
+        Bitmap Crop2(Bitmap image, int w, int h, int x, int y)
+        {
+            Rectangle rect = new Rectangle(x, y, w, h);
+            return image.Clone(rect, image.PixelFormat);
+        }
+
+        private async void button_Triming_Click(object sender, EventArgs e)
+        {
+
+            int stride = (int)numericUpDown1.Value;
+            int Wt = 224;
+            int Ht = 224;
+
+            Bitmap imgSrc = new Bitmap(this.OriginFilePath);
+
+            int Wo = imgSrc.Width;
+            int Ho = imgSrc.Height;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int cnt = 0;
+
+            await Task.Run(() =>
+            {
+                // 
+                // ここで重たい処理をする
+                // 
+                // 
+
+                for (int x = 0; x < (Wo - Wt); x += stride)
+                {
+                    for (int y = 0; y < (Ho - Ht); y += stride)
+                    {
+                        //this.textBox_Debug.Text = $"x{x},y{y} start";
+
+                        string savefilename = $"_{Wt}x{Ht}s{stride}_x{x}y{y}.jpg";
+                        string SaveFilePath = Path.Combine(SaveDirPath, Path.GetFileName(OriginFilePath) + savefilename);
+
+                        imgSrc = new Bitmap(this.OriginFilePath);
+
+                        Bitmap imgDst = Crop2(imgSrc, Wt, Ht, x, y);
+
+                        //Bitmap imgDst = image2(imgSrc);
+                        imgDst.Save(SaveFilePath, ImageFormat.Jpeg);
+                        imgDst.Dispose();
+                        imgSrc.Dispose();
+
+
+                        this.BeginInvoke(new Action(() =>
+                        {
+                            this.textBox_Debug.Text = $"{cnt} :: x{x},y{y} {sw.ElapsedMilliseconds}ms";
+                        }), null);
+
+                        cnt++;
+
+                    }
+                }
+
+            });
+
+            sw.Stop();
+            this.textBox_Debug.Text = $"{cnt}: Elapsed time = {sw.ElapsedMilliseconds} ms";
+
+
+        }
+
+        private void button_SaveDir_Click(object sender, EventArgs e)
+        {
+
+            //FolderBrowserDialogクラスのインスタンスを作成
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            //上部に表示する説明テキストを指定する
+            fbd.Description = "フォルダを指定してください。";
+            //ルートフォルダを指定する
+            //デフォルトでDesktop
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            //最初に選択するフォルダを指定する
+            //RootFolder以下にあるフォルダである必要がある
+            fbd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            //ユーザーが新しいフォルダを作成できるようにする
+            //デフォルトでTrue
+            fbd.ShowNewFolderButton = true;
+
+            //ダイアログを表示する
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                //選択されたフォルダを表示する
+                Console.WriteLine(fbd.SelectedPath);
+                this.label_Savedir.Text = fbd.SelectedPath;
+            }
+            else
+            {
+                MessageBox.Show("正しいディレクトリを選択してください");
+                return;
+            }
+
+            this.SaveDirPath = fbd.SelectedPath;
+            imageList1.Images.Clear();
+            listView_TrimedImages.Clear();
+
+
+        }
+
+        private void button_ListUpdate_Click(object sender, EventArgs e)
+        {
+            ////////////////////////////////////////////////////
+            string imageDir = this.SaveDirPath;
+            string[] jpgFiles = System.IO.Directory.GetFiles(imageDir, "*.jpg");
+
+            int width = 50;
+            int height = 50;
+
+            // 初期化
+            imageList1.ImageSize = new Size(width, height);
+            listView_TrimedImages.LargeImageList = imageList1;
+
+
+            for (int i = 0; i < jpgFiles.Length; i++)
+            {
+
+                if (imageList1.Images.ContainsKey(Path.GetFileName(jpgFiles[i])))
+                {
+                    continue;
+                }
+
+                Image original = Bitmap.FromFile(jpgFiles[i]);
+                Image thumbnail = createThumbnail(original, width, height);
+
+                //imageList1.Images.Add(thumbnail);
+                imageList1.Images.Add(Path.GetFileName(jpgFiles[i]), thumbnail);
+                listView_TrimedImages.Items.Add(Path.GetFileName(jpgFiles[i]), i);
+
+                original.Dispose();
+                thumbnail.Dispose();
+            }
         }
     }
 }
